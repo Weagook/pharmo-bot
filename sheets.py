743766e7d3d519fd9a511654d19044c69166791d
Weagook <weagook@gmail.com>
2023-07-13 00:02:35 +0600

    Improved functions for finding the right answer

--- sheets.py
+++ sheets.py
@@ -3,42 +3,43 @@ import httplib2
 import apiclient.discovery
 from oauth2client.service_account import ServiceAccountCredentials	
 
+def give_acces(email) -> None:
+    '''Выдача доступа'''
+    driveService = apiclient.discovery.build('drive', 'v3', http = httpAuth) # Выбираем работу с Google Drive и 3 версию API
+    access = driveService.permissions().create(
+        fileId = spreadsheetId,
+        body = {'type': 'user', 'role': 'writer', 'emailAddress': email},  # Открываем доступ на редактирование
+        fields = 'id'
+    ).execute()
+
+def get_data() -> list:
+    result = service.spreadsheets().values().get(
+    spreadsheetId=spreadsheetId,
+    range='Главный лист!A2:B100' 
+    ).execute()
+    return result
+
+def database_search(request) -> str or bool:
+    result = get_data()
+    request = request.lower().replace(',', '').replace('?', '').replace('!', '').replace(' ', '')
+    for row in result['values']:
+        if row[0].lower().replace(',', '').replace('?', '').replace('!', '').replace(' ', '') == request:
+            return row[1]
+    return False
+
+
 CREDENTIALS_FILE = 'credentials.json' 
-
-# Читаем ключи из файла
 credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive'])
-
 httpAuth = credentials.authorize(httplib2.Http()) # Авторизуемся в системе
 service = apiclient.discovery.build('sheets', 'v4', http = httpAuth) # Выбираем работу с таблицами и 4 версию API 
-
-# spreadsheet = service.spreadsheets().create(body = {
-#     'properties': {'title': 'Вопросы и ответы', 'locale': 'ru_RU'},
-#     'sheets': [{'properties': {'sheetType': 'GRID',
-#                                'sheetId': 0,
-#                                'title': 'Главный лист',
-#                                'gridProperties': {'rowCount': 100, 'columnCount': 15}}}]
-# }).execute()
-# spreadsheetId = spreadsheet['spreadsheetId'] # сохраняем идентификатор файла
-# print('https://docs.google.com/spreadsheets/d/' + spreadsheetId)
 spreadsheetId = '18Pps6sDbv4AqDyy2QyXLrW1Ext4Aun8G9eAN6roQf6M'
+sheetId = 0 
 
-# # Выдача доступа
-# driveService = apiclient.discovery.build('drive', 'v3', http = httpAuth) # Выбираем работу с Google Drive и 3 версию API
-# access = driveService.permissions().create(
-#     fileId = spreadsheetId,
-#     body = {'type': 'user', 'role': 'writer', 'emailAddress': 'weagook@gmail.com'},  # Открываем доступ на редактирование
-#     fields = 'id'
-# ).execute()
+# Проверка, возвращает ли значение из таблицы
+ans = database_search('и так далее??')     
+print(ans)
 
-# # Получаем список листов, их Id и название
-# spreadsheet = service.spreadsheets().get(spreadsheetId = spreadsheetId).execute()
-# sheetList = spreadsheet.get('sheets')
-# for sheet in sheetList:
-#     print(sheet['properties']['sheetId'], sheet['properties']['title'])
-    
-# sheetId = sheetList[0]['properties']['sheetId']
 
-sheetId = 0
 
 # results = service.spreadsheets().batchUpdate(spreadsheetId = spreadsheetId, body = {
 #   "requests": [
@@ -89,9 +90,13 @@ sheetId = 0
 #     ]
 # }).execute()
 
-result = service.spreadsheets().values().get(
-    spreadsheetId=spreadsheetId,
-    range='Главный лист!A2:B100'  # Укажите диапазон ячеек для чтения
-).execute()
 
-print(result['values'])
+# spreadsheet = service.spreadsheets().create(body = {
+#     'properties': {'title': 'Вопросы и ответы', 'locale': 'ru_RU'},
+#     'sheets': [{'properties': {'sheetType': 'GRID',
+#                                'sheetId': 0,
+#                                'title': 'Главный лист',
+#                                'gridProperties': {'rowCount': 100, 'columnCount': 15}}}]
+# }).execute()
+# spreadsheetId = spreadsheet['spreadsheetId'] # сохраняем идентификатор файла
+# print('https://docs.google.com/spreadsheets/d/' + spreadsheetId)
